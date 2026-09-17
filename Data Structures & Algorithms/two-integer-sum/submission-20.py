525a597448afd1d1259b1963514ef189909e7ac0
saumyasoc-cmd <saumyasoc@gmail.com>
2026-09-18 00:14:26 +0530

    Add: two-integer-sum - submission-20

---
+++ Data Structures & Algorithms/two-integer-sum/submission-20.py
@@ -0,0 +1,18 @@
+class Solution:
+    #brute
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        values=[(num,i)for i, num in enumerate(nums)]
+        values.sort()
+        p1=0
+        p2=len(values)-1
+        for i in range (len(nums)-1):
+            if values[p1][0]+values[p2][0]>target:
+                p2-=1
+            elif values[p1][0]+values[p2][0]<target:
+                p1+=1
+            else:
+                return sorted([values[p1][1],values[p2][1]])
+        return []
+
+
+        
